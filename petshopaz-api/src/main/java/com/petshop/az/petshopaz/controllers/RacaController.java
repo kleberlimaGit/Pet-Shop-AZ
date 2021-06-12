473e2fa14075bd9cbda1a7bb6d2ff102ace413fb
kleber lima <kleber.lima@pm.ba.gov.br>
2021-06-12 16:41:07 -0300

    CRUD racas

---
+++ petshopaz-api/src/main/java/com/petshop/az/petshopaz/controllers/RacaController.java
@@ -0,0 +1,74 @@
+package com.petshop.az.petshopaz.controllers;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.petshop.az.petshopaz.entidades.dto.RacaDTO;
+import com.petshop.az.petshopaz.servicos.RacaService;
+
+@RestController
+@RequestMapping(value = "/racas")
+public class RacaController {
+
+	@Autowired
+	private RacaService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<RacaDTO>> buscarTodosRacas(
+			@RequestParam(value = "page", defaultValue = "0") Integer pagina,
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linhasPorPagina,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "tipoRaca") String orderBy){
+			PageRequest pageRequest = PageRequest.of(pagina, linhasPorPagina, Direction.valueOf(direction), orderBy);
+			
+			Page<RacaDTO> page = service.BuscaPaginada(pageRequest);
+			
+			return ResponseEntity.ok(page);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<RacaDTO> buscarPorId(@PathVariable Long id) {
+		RacaDTO racaDto = service.buscarPorId(id);
+		
+		return ResponseEntity.ok(racaDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<RacaDTO> cadastrarRaca(@Valid @RequestBody RacaDTO racaDto){
+		racaDto = service.inserirRaca(racaDto);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(racaDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(racaDto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<RacaDTO> atualizarRaca(@Valid @RequestBody RacaDTO racaDto, @PathVariable Long id){
+		racaDto = service.atualizarRaca(id, racaDto);
+		return ResponseEntity.ok().body(racaDto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<RacaDTO> deletarRaca(@PathVariable Long id){
+		service.deletarRaca(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
---
+++ petshopaz-api/src/main/java/com/petshop/az/petshopaz/servicos/RacaService.java
@@ -0,0 +1,75 @@
+package com.petshop.az.petshopaz.servicos;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.petshop.az.petshopaz.entidades.Raca;
+import com.petshop.az.petshopaz.entidades.dto.RacaDTO;
+import com.petshop.az.petshopaz.repositorios.RacaRepositorio;
+import com.petshop.az.petshopaz.servicos.exceptions.DatabaseException;
+import com.petshop.az.petshopaz.servicos.exceptions.ResourceNotFoundException;
+
+@Service
+@Transactional
+public class RacaService {
+	
+	@Autowired
+	private RacaRepositorio repositorio;
+	
+	@Transactional(readOnly = true)
+	public Page<RacaDTO> BuscaPaginada(PageRequest pageRequest){
+		Page<Raca> page = repositorio.findAll(pageRequest);
+		
+		return page.map(raca -> new RacaDTO(raca));
+	}
+	
+	@Transactional(readOnly = true)
+	public RacaDTO buscarPorId(Long id) {
+		Optional<Raca> obj = repositorio.findById(id);
+		Raca raca = obj.orElseThrow(() -> new ResourceNotFoundException("Raca não encontrado"));
+		
+		return new RacaDTO(raca);
+	}
+	
+	public RacaDTO inserirRaca(RacaDTO dto) {
+		Raca raca = new Raca();
+		raca.setTipoRaca(dto.getTipoRaca());
+		raca = repositorio.save(raca);
+		
+		return new RacaDTO(raca);
+	}
+	
+	public RacaDTO atualizarRaca(Long id, RacaDTO dto) {
+		try {
+			Raca raca = repositorio.getOne(id);
+			raca.setTipoRaca(dto.getTipoRaca());
+			raca = repositorio.save(raca);
+			return new RacaDTO(raca);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Raca não encontrado. " + id);
+		}
+	}
+	
+	public void deletarRaca(Long id) {
+		try {
+			repositorio.deleteById(id);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Raca não encontrado. " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de integridade");
+		}
+	}
+	
+	
+}
