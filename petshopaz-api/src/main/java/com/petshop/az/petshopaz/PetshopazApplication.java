871e622f9dbb6b68831115a42e6f7b908728c92b
kleber lima <kleber.lima@pm.ba.gov.br>
2021-06-11 22:13:10 -0300

    inicio do projeto

---
+++ petshopaz-api/src/main/java/com/petshop/az/petshopaz/PetshopazApplication.java
@@ -0,0 +1,13 @@
+package com.petshop.az.petshopaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopazApplication.class, args);
+	}
+
+}
---
+++ petshopaz-api/src/test/java/com/petshop/az/petshopaz/PetshopazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshop.az.petshopaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
