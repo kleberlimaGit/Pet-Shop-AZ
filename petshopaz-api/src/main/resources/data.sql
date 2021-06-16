3f432e3632794c22d7f374cdc312363787f81912
kleber lima <kleber.lima@pm.ba.gov.br>
2021-06-16 16:41:16 -0300

    Pequenos ajustes

--- petshopaz-api/src/main/resources/data.sql
+++ petshopaz-api/src/main/resources/data.sql
@@ -1,7 +1,44 @@
-INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Kleber Lima', '051.232.875-74', '71 98826-2661');
-INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Pedro Ricardo', '051.875.875-74', '71 98276-2661');
-INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Denise Souza', '051.745.875-74', '71 96982-6661');
-INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Andre Ferraz', '051.35.875-74', '71 99826-9861');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Benício Raimundo Brito', '887.683.330-72', '(86) 98978-2041');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Analu Julia Porto', '998.363.540-26', '(42) 99151-2362');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Maya Luzia Letícia Fogaça', '084.058.910-72', '(32) 98493-1048');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Isaac Guilherme Nunes', '981.471.200-01', '(79) 98460-7654');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Sônia Marina Gonçalves', '792.119.090-15', '(67) 98982-1643');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Luna Raquel Cecília Ramos', '210.364.740-89', '(61) 99363-0595');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Cláudia Heloisa Valentina Costa', '141.378.340-67', '(81) 99134-5903');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Joana Rosa Kamilly dos Santos', '156.964.020-38', '(92) 99416-7678');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Nair Sônia Natália Rocha', '305.966.440-43', '(54) 99272-9786');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Helena Emilly Sandra Lima', '364.425.290-45', '(12) 98293-1888');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Andreia Emily da Rocha', '558.890.040-50', '(95) 99754-2223');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Cristiane Andreia Tatiane Moreira', '652.561.790-12', '(85) 99993-5918');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Leonardo Giovanni Galvão', '508.934.050-19', '(95) 98865-7420');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Otávio Osvaldo Luiz Rezende', '026.189.980-57', '(64) 98153-1527');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Débora Fátima Freitas', '290.199.610-80', '(61) 99793-7213');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Victor Heitor Rezende', '923.664.550-03', '(21) 98335-5367');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Levi Ruan Cauã Nunes', '473.258.280-04', '(69) 98929-7138');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Raquel Silvana Lara Barbosa', '293.996.370-34', '(19) 99425-5484');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Rebeca Stella Porto', '344.258.480-98', '71 99826-9861');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Davi Breno Miguel Silveira', '717.905.740-13', '(91) 98344-6314');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Edson Bernardo Ricardo Porto', '273.541.880-42', '(84) 99271-5357');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Josefa Eloá Alana das Neves', '872.237.920-78', '(65) 98137-3367');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Ayla Betina Sebastiana Silva', '714.487.040-81', '(68) 98877-1527');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Diogo Rafael Yago da Mota', '545.498.890-12', '(69) 98444-3309');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Isabella Vanessa Rafaela da Rosa', '296.673.330-60', '(32) 99671-4328');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Carlos Hugo Eduardo de Paula', '935.926.400-89', '(54) 98276-7448');
+INSERT INTO CLIENTE (nome,cpf,telefone) VALUES ('Catarina Teresinha Heloise dos Santos', '720.620.530-53', '(71) 98127-6277');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 INSERT INTO RACA (tipo_raca) VALUES ('PASTOR ALEMÃO');
--- petshopaz-app/src/core/pages/petshop/index.tsx
+++ petshopaz-app/src/core/pages/petshop/index.tsx
@@ -13,12 +13,10 @@ import ClientCard from "./components/ClienteCard";
 import Pagination from "@material-ui/lab/Pagination";
 import "./styles.css";
 
-
-
-
 const Petshop = () => {
   const [clienteResponse, setClienteResponse] = useState<ClienteResponse>();
-  const initialPage = () => Number(window.sessionStorage.getItem("page") || null)
+  const initialPage = () =>
+    Number(window.sessionStorage.getItem("page") || null);
   const [activePage, setActivePage] = useState(initialPage);
   const [filtro, setFiltro] = useState("");
 
@@ -27,10 +25,11 @@ const Petshop = () => {
       page: activePage,
       filtro,
     };
-    window.sessionStorage.setItem("page",String(activePage))
-    makeRequest({ url: "/clientes", params }).then((response) =>
-      setClienteResponse(response.data)
-    );
+    window.sessionStorage.setItem("page", String(activePage));
+    makeRequest({ url: "/clientes", params })
+      .then((response) => setClienteResponse(response.data))
+      .finally(() => {
+      });
   }, [activePage, filtro]);
 
   useEffect(() => {
@@ -38,30 +37,28 @@ const Petshop = () => {
   }, [buscarCards]);
 
   const handleChangeFilter = (filtro: string) => {
-    setActivePage(0)
+    setActivePage(0);
     setFiltro(filtro);
   };
 
   const onRemove = (idCliente: number) => {
     makeRequest({ url: `/clientes/${idCliente}`, method: "DELETE" })
-        .then(() => {
-          document.location.reload()
-        })
-        .catch(() => {
-            
-        })
-}
+      .then(() => {
+        document.location.reload();
+      })
+      .catch(() => {});
+  };
 
-  const handleResetSession = () => window.sessionStorage.removeItem("page")
-
-
-  
+  const handleResetSession = () => window.sessionStorage.removeItem("page");
 
   return (
     <div className="container" style={{ marginTop: "8rem" }}>
-
       <div className="d-flex justify-content-center align-items-center mb-5 mt-2 flex-sm-row flex-column">
-        <Link to="/clientes/cadastrar" className="text-decoration-none"  onClick={handleResetSession}>
+        <Link
+          to="/clientes/cadastrar"
+          className="text-decoration-none"
+          onClick={handleResetSession}
+        >
           <p className="mr-sm-5 mr-0 h4 text-center link-style">
             Cadastrar Cliente{" "}
             <FontAwesomeIcon icon={faUserPlus} className="ml-1" />
@@ -70,7 +67,12 @@ const Petshop = () => {
 
         <Link to="racas/cadastrar" className="text-decoration-none">
           <p className=" h4 text-center mt-1 link-style">
-            Cadastrar Raça <FontAwesomeIcon icon={faBone} className="ml-1"  onClick={handleResetSession}/>
+            Cadastrar Raça{" "}
+            <FontAwesomeIcon
+              icon={faBone}
+              className="ml-1"
+              onClick={handleResetSession}
+            />
           </p>
         </Link>
       </div>
@@ -79,17 +81,18 @@ const Petshop = () => {
         Lista de Clientes
       </h3>
       <div className="d-flex justify-content-md-end align-items-center justify-content-center mt-md-2 mt-5">
-        <input className="input-filter"
+        <input
+          className="input-filter"
           type="text"
           placeholder="Pesquisar cliente por nome, pet ou raça"
           onChange={(event) => handleChangeFilter(event.target.value)}
           value={filtro}
         />
-        <FontAwesomeIcon icon={faSearch} className="ml-1 text-primary search"/>
+        <FontAwesomeIcon icon={faSearch} className="ml-1 text-primary search" />
       </div>
       <div className="row">
         {clienteResponse?.content.map((cliente) => (
-          <ClientCard cliente={cliente} key={cliente.id} onRemove={onRemove}/>
+          <ClientCard cliente={cliente} key={cliente.id} onRemove={onRemove} />
         ))}
       </div>
 
@@ -107,6 +110,8 @@ const Petshop = () => {
           </div>
         </>
       )}
+
+
     </div>
   );
 };
